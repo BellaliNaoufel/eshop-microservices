b1aec23af06a31ff305744107ad7d47cb7b0df7d
bellali naoufel <bellalinaoufel@gmail.com>
2024-10-01 21:43:08 +0200

    marten add and get (just test)

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,13 +1,17 @@
+using Catalog.API.Models;
 using Marten;
+using Weasel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add Services to container.
 builder.Services.AddMarten(options =>
 {
     options.Connection(builder.Configuration.GetConnectionString("Default")!);
-    options.UseSystemTextJsonForSerialization();
+    options.UseSystemTextJsonForSerialization(enumStorage: EnumStorage.AsString, casing: Casing.Default);
+    options.AutoCreateSchemaObjects = AutoCreate.CreateOrUpdate; // TODO: only on dev
 })
-    .AssertDatabaseMatchesConfigurationOnStartup();
+    .AssertDatabaseMatchesConfigurationOnStartup()
+    .UseLightweightSessions();
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
@@ -19,11 +23,26 @@ app.MapGet("/load", (IQuerySession session) =>
 });
 app.MapGet("/create", async (IDocumentSession session) =>
 {
-    session.Store(new Product(Guid.NewGuid(), $"Product{new Random().Next(1, 1000)}", $"Description{new Random().Next(1, 1000)}"));
+    session.Store(new Product
+    {
+        Id = Guid.NewGuid(),
+        Name = $"Product{new Random().Next(1, 100000)}",
+        Description = $"Description{new Random().Next(1, 1000)}",
+        Price = (new Random().Next(1, 100000)) / 10,
+        Categories = new List<Category>
+        {
+            new Category {
+                Id= Guid.NewGuid(),
+                Name=$"Category{new Random().Next(1, 1000)}"
+            },
+             new Category {
+                Id= Guid.NewGuid(),
+                Name=$"Category{new Random().Next(1, 1000)}"
+            }
+        }
+    });
     await session.SaveChangesAsync();
     return "Product Created!";
 });
 
 app.Run();
-
-public record Product(Guid Id, string Name, string Description);
